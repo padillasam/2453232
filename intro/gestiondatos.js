8140d0548c7f89f3bee0ba52fa74d8d04192b9b4
padillasam <panarick@gmail.com>
2022-07-25 10:22:15 -0500

    segunda version estructuras de control

--- intro/gestiondatos.js
+++ intro/gestiondatos.js
@@ -1,1 +1,48 @@
-console.log('Yo amo la programación de software')
+var x1=1;
+console.log('Yo amo la programación de software');
+let y=10;
+console.log("valor de x ="+x);
+console.log(`valor de x = ${x}`);
+//aritmeticos: + - * / % -> resultado numérico
+//relacionales: ==, ===, <, >. >=. <=, != ->resultado falso o verdadero
+//lógico &&, ||, ! ->resultado falso o verdadero
+// x>=5 && x<=20
+// x>=5 || x<=20 
+//x=true    !x cambia el valor de verdad
+//asignación-> =, +=, -=,*=, /=, %=
+//ESTRUCTURAS DE CONTROL DE FLUJO DE DATOS
+//CONDICIONALES
+var x=5;
+var z=7;
+if (x>0) {
+    console.log(`${x} es positivo`)
+} else if(x<0) {
+    console.log(`${x} es negativo`)
+}else{
+    console.log(`${x} es neutro`)
+}
+
+var selector;
+console.log('1-sumar');
+console.log('2-restar');
+console.log('3-multiplicar');
+console.log('4-salir');
+selector=-7;
+
+switch (selector) {
+    case 1:
+        console.log(`La suma es= ${x+z}`)
+    break;
+    case 2:
+        console.log(`La resta es= ${x-z}`)
+    break;
+    case 3:
+        console.log(`el producto es = ${x*z}`)
+    break;
+    case 4:
+        console.log('chao')
+    break;
+    default:
+        console.log('ERROR');
+    break;
+}
